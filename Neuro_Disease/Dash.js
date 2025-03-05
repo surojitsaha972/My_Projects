f4929e0dc2a74d0fb4c1f1603ac0da9fc7b518dc
surojitsaha972 <sahasurojit972@gmail.com>
2025-03-05 20:24:49 +0530

    Modified

--- Neuro_Disease/Dash.js
+++ Neuro_Disease/Dash.js
@@ -3,9 +3,12 @@ function Loginpage(){
 }
 
 const links=document.querySelectorAll('.link_tab');
+const tabs=document.querySelectorAll('.alpha');
 links.forEach(link => {
-    link.addEventListener('click', () =>{
-        document.querySelector('.active')?.classList.remove('active');
+    link.addEventListener('click', () => {
+        link.document.querySelector('.active')?.classList.remove('active');
         link.classList.add('active');
+        // tabs.document.querySelector('.active')?.classList.remove('active');
+        // tabs.classList.add('active');
     })
 })
