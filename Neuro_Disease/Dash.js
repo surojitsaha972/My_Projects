34f0fcfbb000defc4162a19407776c67084aa1ab
surojitsaha972 <sahasurojit972@gmail.com>
2025-03-10 09:30:15 +0530

    Modified Dashboard

--- Neuro_Disease/Dash.js
+++ Neuro_Disease/Dash.js
@@ -2,10 +2,16 @@ function Loginpage(){
     window.location.href = "login.html";
 }
 
-const links=document.querySelectorAll('.link_tab');
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        link.document.querySelector('.active')?.classList.remove('active');
-        link.classList.add('active');
+
+const links=document.querySelectorAll('.alphaorder ul li');
+links.forEach(link =>{
+    link.addEventListener('click', function(){
+        document.querySelectorAll('.alphaorder ul li, .alpha').forEach(el => el.classList.remove('active'));
+        this.classList.add('active');
+        let targetid=this.querySelector('a').getAttribute('data-target');
+        let targetel=document.getElementById(targetid);
+        if(targetel){
+            targetel.classList.add('active');
+        }
     })
 })
