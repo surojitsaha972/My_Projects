aa54f51e011f50cd32db19e20d082bb6779efb95
surojitsaha972 <sahasurojit972@gmail.com>
2025-03-11 19:25:42 +0530

    Modified Files

--- Neuro_Disease/neuro.js
+++ Neuro_Disease/neuro.js
@@ -1,3 +1,25 @@
+// ----- Login Page -----
+
+var a=document.getElementById("login");
+var b=document.getElementById("register");
+
+function login(){
+    a.style.left="5px";
+    b.style.right="-500px";
+    a.style.opacity=1;
+    b.style.opacity=0;
+}
+
+function register(){
+    a.style.left="-510px";
+    b.style.right="5px";
+    a.style.opacity=0;
+    b.style.opacity=1;
+}
+
+
+// ----- Dashboard Page ------
+
 function Loginpage(){
     window.location.href = "login.html";
 }
