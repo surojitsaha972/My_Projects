f5b33deaf0c43508deb7113ca256a31016c18e80
surojitsaha972 <sahasurojit972@gmail.com>
2025-03-27 21:48:26 +0530

    Update Project

--- Neuro_Disease/neuro.js
+++ Neuro_Disease/neuro.js
@@ -37,3 +37,20 @@ links.forEach(link =>{
         }
     })
 })
+
+
+// ----- Disease Details ------
+
+
+// const links=document.querySelectorAll('.alphaorder ul li');
+// links.forEach(link =>{
+//     link.addEventListener('click', function(){
+//         document.querySelectorAll('.alphaorder ul li, .alpha').forEach(el => el.classList.remove('active'));
+//         this.classList.add('active');
+//         let targetid=this.querySelector('a').getAttribute('data-target');
+//         let targetel=document.getElementById(targetid);
+//         if(targetel){
+//             targetel.classList.add('active');
+//         }
+//     })
+// })
