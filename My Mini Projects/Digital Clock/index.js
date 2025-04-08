263500bd468ef2b9f15f5fce4d26e5aad98c02af
surojitsaha972 <sahasurojit972@gmail.com>
2025-04-08 14:16:17 +0530

    make a new small project

--- My Mini Projects/Digital Clock/index.js
+++ My Mini Projects/Digital Clock/index.js
@@ -1,3 +1,17 @@
 const t = document.getElementById("time");
 const tf = document.getElementById("timeformat");
 
+document.addEventListener('DOMContentLoaded',()=>{
+    setInterval(showTime,1000);
+});
+const showTime=()=>{
+    let date=new Date();
+    let hr=date.getHours();
+    let min=date.getMinutes();
+    let sec=date.getSeconds();
+    hr=hr<10? `0${hr}`:hr;
+    min=min<10? `0${min}`:min;
+    sec=sec<10? `0${sec}`:sec;
+    t.innerHTML=`${hr} : ${min} : ${sec}`;
+    tf.innerHTML=hr>12? "PM":"AM";
+};
